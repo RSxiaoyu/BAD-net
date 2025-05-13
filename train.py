65aae0b8678511320e234e7113a48783cad74f4b
_xiaoyu <2872127712@qq.com>
2025-05-13 13:12:16 +0800

    feat(train): :sparkles: add results visulization

--- train.py
+++ train.py
@@ -1,53 +1,77 @@
+from matplotlib import pyplot as plt
 import torch
 from tqdm import tqdm
+from dataset import test_loader, train_loader
 from model import BADNet
-from dataset import train_loader,test_loader
+
 
 def train_loop(model, data_loader, optimizer, epoch, alpha):
     model.train()
-    loss = 0
+    loss=0
     scaler = torch.amp.GradScaler()
     for images, targets in tqdm(data_loader):
         images = images.to(device)
         targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
-        with torch.amp.autocast('cuda'):
+        optimizer.zero_grad()
+        # with torch.amp.autocast('cuda'):
+        det_loss = model(images, targets)
+        hr_loss = model.backbone.hr_loss
+        tmp_loss = sum(det_loss.values()) + alpha * hr_loss
+        loss+=tmp_loss
+        # 反向传播
+        # scaler.scale(tmp_loss).backward()
+        # scaler.step(optimizer)
+        # scaler.update()
+
+        tmp_loss.backward()
+        optimizer.step()
+    train_loss.append(loss.item())
+    print(f"\nEpoch {epoch}, Train Loss: {loss.item():.4f}")
+
+def test_loop(model, data_loader, alpha):
+    # model.eval()
+    loss=0
+    scaler = torch.amp.GradScaler()
+    with torch.no_grad():
+        for images, targets in tqdm(data_loader):
+            images = images.to(device)
+            targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
             det_loss = model(images, targets)
             hr_loss = model.backbone.hr_loss
-            loss = sum(det_loss.values()) + alpha * hr_loss
-        
-        # 反向传播
-        optimizer.zero_grad()
-        scaler.scale(loss).backward()
-        scaler.step(optimizer)
-        scaler.update()
-
-        # optimizer.zero_grad()
-        # loss.backward()
-        # optimizer.step()
-        
-    print(f"\nEpoch {epoch}, Total Loss: {loss.item():.4f}")
-
-
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-num_classes = 6
-epochs = 1
-batch_size = 16
-lr = 1e-4
-weight_decay = 1e-4
-alpha = 0.001
-
-
-model = BADNet(num_classes=num_classes).to(device)
-optimizer = torch.optim.Adam(model.parameters(), lr=lr, weight_decay=weight_decay) 
-scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=30, gamma=0.1)
-
-print(device)
-for epoch in tqdm(range(epochs)):
-    train_loop(model, test_loader(), optimizer, epoch, alpha=alpha)
-    scheduler.step()  # 更新学习率
+            tmp_loss = sum(det_loss.values()) + alpha * hr_loss
+            loss+=tmp_loss
+    test_loss.append(loss.item())
+    print(f"\nEpoch {epoch}, Test Loss: {loss.item():.4f}")
     
-    # 间隔迭代训练策略（论文中的Interval Iterative Strategy，简化实现）
-    # if epoch % 2 == 1:  # 奇数epoch生成精炼数据集（示例逻辑，需根据论文Algorithm 1完整实现）
-    #     # 此处需添加生成精炼数据集的逻辑（如对去雾图像添加不同程度的雾）
-    #     # 简化示例：仅使用原始数据集，完整实现需参考论文数据增强算法
-    #     pass
+if __name__=='__main__':
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    num_classes = 6
+    epochs = 20
+    batch_size = 8
+    lr = 1e-4
+    weight_decay = 1e-4
+    alpha = 0.001
+    train_loss=[]
+    test_loss=[]
+
+    model = BADNet(num_classes=num_classes).to(device)
+    optimizer = torch.optim.Adam(model.parameters(), lr=lr, weight_decay=weight_decay) 
+    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=30, gamma=0.1)
+    trainloader=train_loader(batch_size,range(50))
+    testloader=test_loader(batch_size,range(25))
+    print(f'Model running on {device}')
+    plt.ion()
+    for epoch in tqdm(range(epochs)):
+        train_loop(model, trainloader, optimizer, epoch, alpha=alpha)
+        test_loop(model,testloader,alpha=alpha)
+        scheduler.step()  # 更新学习率
+        plt.cla()
+        plt.plot(train_loss, label='Train Loss', color='blue')
+        plt.plot(test_loss, label='Test Loss', color='red')  # 虚线区分
+        plt.xlabel('Epoch')
+        plt.ylabel('Loss')
+        plt.legend()
+        plt.grid(True, linestyle='--', alpha=0.7)
+        plt.pause(0.1)
+    plt.ioff()
+    plt.show()
