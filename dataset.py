35720f9660ac2f439750521aa6ff1e98830e272c
_xiaoyu <2872127712@qq.com>
2025-05-13 18:12:55 +0800

    feat(dataset): :sparkles: add add_haze function to generate haze dataset

--- dataset.py
+++ dataset.py
@@ -1,7 +1,7 @@
 import torch
 from pprint import pprint
 from torchvision.transforms import v2
-from torch.utils.data import DataLoader,ConcatDataset,Subset
+from torch.utils.data import DataLoader,ConcatDataset,Subset,Dataset
 from torchvision.datasets import VOCDetection,wrap_dataset_for_transforms_v2
 
 
@@ -10,7 +10,14 @@ classes = [
     'diningtable', 'dog', 'horse', 'motorbike', 'person', 'pottedplant', 'sheep', 'sofa', 'train', 'tvmonitor'
 ] # index = class + 1
 target_classes=['','person','bicycle','motorbike','bus','car']
-
+collate_fn=lambda batch: (torch.stack([x[0] for x in batch]),[x[1] for x in batch])
+def add_haze(dehazed_image, level=0.05):
+    C, H, W = dehazed_image.shape
+    y, x = torch.meshgrid(torch.arange(H,device=dehazed_image.device), torch.arange(W,device=dehazed_image.device), indexing='ij')
+    d = torch.sqrt((x - W/2)**2 + (y - H/2)**2) / max(H, W)
+    td = torch.exp(-level * d)
+    hazed_image = dehazed_image * td + 0.5 * (1 - td)
+    return hazed_image
 
 class FilterCategories(torch.nn.Module):
     def __init__(self, target_classes):
@@ -23,6 +30,17 @@ class FilterCategories(torch.nn.Module):
         target['boxes'] = target['boxes'][mask]
         target['labels'] = torch.tensor([self.index_map[item.item()] for item in target['labels'][mask]],dtype=torch.int64)
         return image,target
+    
+class RandomHazeTransform(torch.nn.Module):
+    def __init__(self, max_level=0.2, add_prob=2/3):
+        super().__init__()
+        self.max_level = max_level
+        self.add_prob = add_prob
+    
+    def forward(self, image, target):
+        if torch.rand(1) < self.add_prob:
+            image=add_haze(image)
+        return image, target  # 返回加雾后的图像和原标签
 
 
 def voc_dataset(root='./dataset',year='2007',image_set='test'):
@@ -43,14 +61,15 @@ def train_loader(batch_size=8,subset=None):
     print('VOC 2012 trainval loaded.')
     voc_trainval=ConcatDataset([voc2007_trainval,voc2012_trainval])
     if subset:
-        voc_trainval=Subset(voc_trainval,subset)
+        voc_trainval=Subset(voc2007_trainval,subset)
     print('VOC trainval ready.')
     return DataLoader(
         voc_trainval,
         batch_size=batch_size,
         shuffle=True,
-        collate_fn=lambda batch: (torch.stack([x[0] for x in batch]),[x[1] for x in batch]),
+        collate_fn=collate_fn,
     )
+
 def test_loader(batch_size=8,subset=None):
     voc_test=voc_dataset()
     if subset:
@@ -59,5 +78,37 @@ def test_loader(batch_size=8,subset=None):
     return DataLoader(
         voc_test,
         batch_size=batch_size,
-        collate_fn=lambda batch: (torch.stack([x[0] for x in batch]),[x[1] for x in batch]),
+        collate_fn=collate_fn,
+    )
+
+    
+class refined_dataset(Dataset):
+    def __init__(self, ori_dataset, dehazing_module, device, num_levels):
+        self.ori_dataset = ori_dataset
+        self.dehazing_model = dehazing_module.eval()
+        self.num_levels = num_levels
+        self.device=device
+    def __len__(self):
+        return len(self.ori_dataset) * self.num_levels
+    
+    def __getitem__(self, index):
+        ori_index = index // self.num_levels
+        level_idx = index % self.num_levels
+        level = 0.05 + 0.01 * level_idx
+        image, target = self.ori_dataset[ori_index]
+        image = image.to(self.device)
+        with torch.no_grad():
+            dehazed_image = self.dehazing_model(image.unsqueeze(0))[0]  # 输出(C, H, W)
+        hazed_image = add_haze(dehazed_image, level)
+        return hazed_image, target
+
+    
+
+def refined_loader(ori_loader,dehazing_module,device,batch_size=8,num_levels=10):
+    dataset=refined_dataset(ori_loader.dataset,dehazing_module,device,num_levels)
+    return DataLoader(
+        dataset,
+        batch_size=batch_size,
+        shuffle=True,
+        collate_fn=collate_fn,
     )
